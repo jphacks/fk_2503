300ca1dee7654b0134dac37f062d178c730df579
rui0421 <rui.04215@icloud.com>
2025-10-18 15:08:59 +0900

    style: Mainにおけるbuttonの配置

--- frontend/src/src/components/Footer/index.tsx
+++ frontend/src/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="w-full h-16 bg-orange-500">
+        <div className="w-full h-16 bg-red-500">
             <div className="flex h-full items-center justify-between">
                 <span className="text-2xl font-bold ml-6">リサクリ</span>
                 <span className="mr-6"></span>
--- frontend/src/src/components/Main/index.tsx
+++ frontend/src/src/components/Main/index.tsx
@@ -1,7 +1,9 @@
 export default function Main() {
     return(
-        <div>
-            
-        </div>
+        <main className="flex-grow flex justify-center items-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded">
+                スタート
+            </button>
+        </main>
     )
 }

45b546d6f986130d123ee1f228a16a0072472ae9
rui0421 <rui.04215@icloud.com>
2025-10-18 16:43:38 +0900

    add: Dockerfileの生成&refactor: Header内のタイトルをprops化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './src/components/Home/index';
+import Home from './src/pages/Home';
+import CameraScreen from './src/pages/Camera';
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/camera' element={<CameraScreen />} />
       </Routes>
     </div>
   )

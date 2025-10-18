e22508cfbae70b0b65cb68e982c42d2dda850713
rui0421 <rui.04215@icloud.com>
2025-10-18 17:55:46 +0900

    router: Camera画面に遷移&Home画面に遷移するボタン設置

--- frontend/src/src/pages/Camera/index.tsx
+++ frontend/src/src/pages/Camera/index.tsx
@@ -1,9 +1,10 @@
 import Header from "../../components/Header";
+import backButton from "../../../assets/img/backPageButton.png"
 
 export default function CameraScreen() {
     return (
         <div className="flex flex-col min-h-screen">
-            <Header title="カメラ" />
+            <Header title={backButton}/>
         </div>
     )
 }

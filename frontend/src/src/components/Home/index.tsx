e61eecae58dc816aa9c521f947c4f28608ecb467
rui0421 <rui.04215@icloud.com>
2025-10-18 15:08:59 +0900

    style: Mainにおけるbuttonの配置

--- frontend/src/src/components/Home/index.tsx
+++ frontend/src/src/components/Home/index.tsx
@@ -1,13 +1,12 @@
 import Footer from "../Footer"
-import Header from "../Header/index"
+import Header from "../Header"
+import Main from "../Main"
 
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow">
-                <p>This is the main content.</p>
-            </main>
+            <Main />
             <Footer />
         </div>
     )

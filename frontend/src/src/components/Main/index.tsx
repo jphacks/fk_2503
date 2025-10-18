406780b1586d3824319a0d5cfe76c2fb76d7e188
rui0421 <rui.04215@icloud.com>
2025-10-18 16:29:04 +0900

    router: スタートボタンを押した際にカメラ画面に遷移

--- frontend/src/src/components/Main/index.tsx
+++ frontend/src/src/components/Main/index.tsx
@@ -1,9 +1,9 @@
+import StartButton from "./StartButton/StartButon";
+
 export default function Main() {
     return(
         <main className="flex-grow flex justify-center items-center">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-8 px-16 rounded">
-                スタート
-            </button>
+            <StartButton />
         </main>
     )
 }

186651b5bbe178735c0babf3c02078a8e5452256
rui0421 <rui.04215@icloud.com>
2025-10-18 16:02:44 +0900

    feature: スライド機能のプロトタイプ実装

--- frontend/src/src/components/Home/index.tsx
+++ frontend/src/src/components/Home/index.tsx
@@ -1,13 +1,13 @@
-import Footer from "../Footer"
 import Header from "../Header"
 import Main from "../Main"
+import SlideShow from "../SlideShow/index"
 
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
-            <Main />
-            <Footer />
+            <SlideShow />
+            <Main />    
         </div>
     )
 }
---
+++ frontend/src/src/components/SlideShow/SlideTutorial/TutorialDetails3.tsx
@@ -0,0 +1,7 @@
+export default function TutorialDetails() {
+    return (
+        <div className="w-full h-[20vh] md:h-[40vh] bg-black text-blue-500 flex justify-center items-center text-4xl font-bold">
+            Slide A
+        </div>
+    )
+}
